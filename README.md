7f9afd4171437aa679addac94f0bbd57b7c3b296
Artemii <pingvixD@yandex.ru>
2017-12-07 01:46:12 +0300

    Fixed some bugs :+1:

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ Tracking face landmarks with IpCam and WebCam.
 * [python 3.6](https://www.python.org/downloads/release/python-363/)
 * imutils
 * [shape_predictor_68_face_landmarks.dat.bz2](http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2)
+* numpy
+* postgresql
 
+To install all libraries use:
+```
+pip install numpy opencv-contrib-python imutils py-postgresql
+```
+
+Download library [dlib](https://pypi.python.org/pypi/dlib/19.7.0) and use:
+```
+pip install path_to\dlib-19.7.0-cp36-cp36m-win_amd64.whl
+```
 
 If shape_predictor located in other path:
 ```
--- face_landmark_detection.py
+++ face_landmark_detection.py
@@ -1,55 +1,69 @@
 # -*- coding: utf-8 -*-
 
-from imutils import resize
-from imutils import face_utils
+from imutils import resize, face_utils
 from threading import Thread
 from argparse import ArgumentParser
-from time import strftime
-from time import gmtime
-from time import time
-from dlib import get_frontal_face_detector
-from dlib import shape_predictor
-from cv2 import imwrite
-from cv2 import imdecode
-from cv2 import VideoCapture
-from cv2 import destroyAllWindows
-from cv2 import imshow
-from cv2 import cvtColor
-from cv2 import COLOR_BGR2GRAY
-from cv2 import circle
-from cv2 import waitKey
+from time import strftime, gmtime, time
+from dlib import get_frontal_face_detector, shape_predictor
+from cv2 import imwrite, imdecode, VideoCapture, destroyAllWindows, \
+                imshow, cvtColor, COLOR_BGR2GRAY, circle, waitKey
 from urllib.request import urlopen
 import numpy as np
+import postgresql
+import os
 
 class SaveThread(Thread):
     #Thread class for saving frame and face landmarks in file
-    def __init__(self, num_ses, num_frame, cur_time, frame, dots):
+    def __init__(self, num_ses, num_frame, cur_time, frame, dots, num_face):
         Thread.__init__(self)
         self.num_ses = num_ses
         self.num_frame = num_frame
         self.cur_time = cur_time
         self.frame = frame
         self.dots = dots
+        self.num_face = num_face
         
     def run(self):
         #Starting thread
         print("[WRITE] Session:", self.num_ses, "Frame:", self.num_frame)
-        image_filename = "images\ses_" + str(self.num_ses) + "_" + "im_" + str(self.num_frame) \
-            + "_" + strftime("%Y_%m_%d_%H-%M-%S", gmtime(self.cur_time)) + ".png"
-        dots_filename = "images\ses_" + str(self.num_ses) + "_" + "dots_" + str(self.num_frame) \
-            + "_" + strftime("%Y_%m_%d_%H-%M-%S", gmtime(self.cur_time)) + ".txt"
-        imwrite(str(image_filename), self.frame)
-        with open(dots_filename, "w") as f:
-            point = 1
-            print("image: ", image_filename, file = f)
-            print("face number: 0", file = f)
-            print("points:", file = f)
-            print("{", file = f)
-            for dot in self.dots:
-                print("\tpoint:", str(point),  "{ x:", str(dot[0]) + "; y:", str(dot[1]), "}", file = f)
-                point += 1
-            print("}", file = f)
-                
+        image_filename = "images\ses_" + str(self.num_ses) + "_im_" + str(self.num_frame) \
+            + "_face_" + str(self.num_face) + "_" + strftime("%Y_%m_%d_%H-%M-%S", gmtime(self.cur_time)) + ".png"
+        dots_filename = "images\ses_" + str(self.num_ses) + "_dots_" + str(self.num_frame) \
+            + "_face_" + str(self.num_face) + "_" + strftime("%Y_%m_%d_%H-%M-%S", gmtime(self.cur_time)) + ".txt"
+        try:
+            imwrite(str(image_filename), self.frame)
+            with open(dots_filename, "w") as f:
+                point = 1
+                print("image: ", image_filename, file = f)
+                print("face number: 0", file = f)
+                print("points:", file = f)
+                print("{", file = f)
+                for dot in self.dots:
+                    print("\tpoint:", str(point),  "{ x:", str(dot[0]) + "; y:", str(dot[1]), "}", file = f)
+                    point += 1
+                print("}", file = f)
+        except:
+            raise Exception("[ERROR] Can't save image to folder!")
+
+    def data_to_db(self):
+        try:
+            db = postgresql.open('')
+            insert_photo = db.prepare("INSERT INTO users (photo, cur_time, num_ses, num_frame) VALUES ($1, $2, $3, $4)")
+            insert_photo(self.frame, self.cur_time, self.num_ses, self.num_frame)
+            select_photo = db.prepare("SELECT CURRVAL('id_photo')")
+            id_photo = select_photo()
+            with postgresql.open(db) as db:
+                for dot in self.dots:
+                    insert_dot = db.prepare("INSERT INTO dots VALUES ($1, $2)")
+                    insert_dot(dot[0], dot[1])
+                    select_dot = db.prepare("SELECT CURRVAL('id_dot')")
+                    id_dot = select_dot()
+                    insert_photo_dot = db.prepare("INSERT INTO photo_dots VALUES ($1, $2)")
+                    insert_photo_dot(id_photo, id_dot)
+        except:
+            raise
+
+             
 
 class IpCamera:
     #Class ipcam stream with urllib
@@ -96,16 +110,15 @@ def stream_face_detection(cam, args):
     except:
         raise Exception("[ERROR] Can't load predictor...")
     while True:
-        # grab the frame from the threaded video stream, resize it to
-        # have a maximum width of 400 pixels, and convert it to
-        # grayscale
+        # grab the frame from the threaded video stream
         frame = cam.get_frame()
-        frame_for_save = frame
-        frame = resize(frame, width=700)
+        frame_for_save = cam.get_frame()
+        #frame = resize(frame, width=700)
         gray = cvtColor(frame, COLOR_BGR2GRAY)
         # detect faces in the grayscale frame
         rects = detector(gray, 0)
         # loop over the face detections
+        itr_face = 0
         for rect in rects:
             # determine the facial landmarks for the face region, then
             # convert the facial landmark (x, y)-coordinates to a NumPy
@@ -113,14 +126,20 @@ def stream_face_detection(cam, args):
             shape = predictor(gray, rect)
             shape = face_utils.shape_to_np(shape)
             if status_saving:
-                cur_time = time()
-                save_thread = SaveThread(itr_ses, itr_frame, cur_time, frame_for_save, shape)
-                save_thread.start()
-                itr_frame += 1
+                try:
+                    cur_time = time()
+                    save_thread = SaveThread(itr_ses, itr_frame, cur_time, frame_for_save, shape, itr_face)
+                    save_thread.start()
+                    itr_frame += 1
+                except:
+                    status_saving = False
+                    itr_ses += 1
+                    itr_frame = 0
             # loop over the (x, y)-coordinates for the facial landmarks
             # and draw them on the image
             for (x, y) in shape:
                 circle(frame, (x, y), 1, (0, 0, 255), -1)
+            itr_face += 1
         # show the frame
         imshow("Stream", frame)
         key = waitKey(1) & 0xFF
@@ -136,6 +155,12 @@ def stream_face_detection(cam, args):
     print("[INFO] Exit...")
 
 def main():
+    images_path = "images"
+    if os.path.exists(images_path):
+        print("[INFO] Directory 'images' exists.")
+    else:
+        os.mkdir("images")
+        print("[INFO] Created directory 'images'.")
     #main func
     # construct the argument parse and parse the arguments
     ap = ArgumentParser()
